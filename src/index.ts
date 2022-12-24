3c10cc5ae79afb65521fb70893af25f59b383f60
Stefan Valentin <khalilravanna@gmail.com>
2022-12-24 15:28:43 -0500

    Split off and return gift entries as well

--- src/index.ts
+++ src/index.ts
@@ -12,31 +12,49 @@ async function main() {
     await page.goto(`file:${htmlFilePath}`);
     await page.waitForSelector('.responsive_page_frame');
     const results = await page.evaluate(() => {
-        const rows = [...document.querySelectorAll('tr')].slice(1);
-        // Get rid of header row
-        return rows.map((row) => {
-            const dateElement = row.querySelector('.wht_date');
-            let gamesNodesQueryList = row
-                .querySelector('.wht_items')
-                // We want just the direct children of the wht_items div. Each one represents a game.
-                ?.querySelectorAll(':scope > div');
+        // Each row is a purchase. Get rid of header row with slice.
+        const rows = [...document.querySelectorAll('tr')]
+            .slice(1)
+            .map((row) => {
+                const dateElement = row.querySelector('.wht_date');
 
-            let gamesNodes =
-                gamesNodesQueryList !== undefined
-                    ? Array.from(gamesNodesQueryList)
-                    : [];
-            // Filter out the payment elements
-            gamesNodes = gamesNodes.filter(
-                (n) => !n.classList.contains('wth_payment')
-            );
+                // The node with the list of games
+                const gamesContainerNode = row.querySelector('.wht_items');
+                // If there's a wth_payment div that's a gift. Filter those out
+                if (gamesContainerNode?.querySelector('.wth_payment')) {
+                    return {
+                        isGift: true,
+                        content: gamesContainerNode
+                            ?.querySelector(':scope > div')
+                            .innerHTML.trim(),
+                    };
+                }
+                //
+                let gamesNodesQueryList =
+                    gamesContainerNode?.querySelectorAll(':scope > div');
 
-            return {
-                date: dateElement?.textContent,
-                games: gamesNodes.map((node) => node.textContent.trim()),
-            };
-        });
+                const gamesNodes =
+                    gamesNodesQueryList !== undefined
+                        ? Array.from(gamesNodesQueryList)
+                        : [];
+
+                return {
+                    isGift: false,
+                    date: dateElement?.textContent,
+                    games: gamesNodes.map((node) => node.textContent.trim()),
+                };
+            });
+
+        return {
+            rowsWithGames: rows.filter((x) => !x.isGift),
+            rowsWithoutGames: rows.filter((x) => x.isGift),
+        };
     });
-    console.log(results);
+    console.log(
+        results.rowsWithGames.length,
+        results.rowsWithoutGames.length,
+        results
+    );
     browser.close();
 }
 
